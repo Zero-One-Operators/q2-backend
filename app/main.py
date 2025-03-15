6f5d6522d6d27a076f9b53101e08429431c8db2c
akeight <allysonkeightley@outlook.com>
2025-03-14 17:06:51 -0700

    Connected LangChain-OpenAI to project with a basic prompt

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from app.routers import chat
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+#from langchain.chat_models import init_chat_model
+#from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 app = FastAPI()
 app.include_router(
@@ -27,6 +32,13 @@ app.add_middleware(
 async def root():
     return {"status": "ok"}
 
+#connect to langchain-openai
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+llm = ChatOpenAI()
 
-class Data(BaseModel):
-    message: str  # The data class will have a message field
+print(llm.invoke("Here is a fun fact about baseball:"))
+
+#commented out only to run llm above without errors
+#class Data(BaseModel):
+ #  message: str  # The data class will have a message field
